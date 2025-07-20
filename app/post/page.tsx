53c0875e42a0c86c3789ba58e968076807d0bbe8
Kresa12 <aurakresa12@gmail.com>
2025-07-20 17:02:32 +0700

    test2

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -5,7 +5,6 @@ const Post = () => {
     <div>
         <h1>Post Page</h1>
         <p>This is the post page content.</p>
-        <p>Here you can add more details about the post.</p>
     </div>
   )
 }
