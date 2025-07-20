e0a2681cdcc98ca2a8802f8311dc243be31dbfb9
fikalalif <fikalgans26@gmail.com>
2025-07-20 17:08:13 +0700

    test3

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -5,6 +5,7 @@ const Post = () => {
     <div>
         <h1>Post Page</h1>
         <p>This is the post page content.</p>
+        <p>maybe u can try to react before u start learn next</p>
     </div>
   )
 }
